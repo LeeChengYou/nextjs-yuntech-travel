9e86e22bb4450fd8deba28b125e907e433ff81e4
Jeff <901126jeff901126@gmail.com>
2024-05-10 16:36:00 +0800

    First

---
+++ src/app/page.js
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Navbar, 
+  NavbarBrand, 
+  NavbarCollapse, 
+  NavbarLink, 
+  NavbarToggle , 
+  Footer,  
+  FooterCopyright, 
+  FooterLink, 
+  FooterLinkGroup,
+  Carousel, 
+  Card,
+
+  Button,
+} from "flowbite-react";
+
+export default function Home() {
+  return (
+    <>
+    <Navbar fluid rounded>
+      <NavbarBrand as={Link} href="https://flowbite-react.com">
+        <img src="/images/vercel.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink as={Link} href="#">
+          About
+        </NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <Card
+      className="max-w-sm"
+      imgAlt="Meaningful alt text for an image that is not purely decorative"
+      imgSrc="/images/blog/image-1.jpg"
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+    </Card>
+    <Carousel slide={false}>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+    </Carousel>
+    </div>
+    <Footer container>
+      <FooterCopyright href="#" by="Flowbite™" year={2022} />
+      <FooterLinkGroup>
+        <FooterLink href="#">About</FooterLink>
+        <FooterLink href="#">Privacy Policy</FooterLink>
+        <FooterLink href="#">Licensing</FooterLink>
+        <FooterLink href="#">Contact</FooterLink>
+      </FooterLinkGroup>
+    </Footer>
+    </>
+  );
+}
