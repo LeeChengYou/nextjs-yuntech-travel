fd7becf6d6bf569b696740ea4714b57b205bb08e
LeeChengYou <901126jeff901126@gmail.com>
2024-05-31 16:42:11 +0800

    create component Card.jsx

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,10 @@
+/** 
+ * @author Cheng You Lee <901126jeff901126@gmail.com>
+ */
+export default function CustomCard({item}){
+    return(
+        <div class="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    ) 
+}
