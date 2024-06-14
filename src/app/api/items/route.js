d0a4d5184fba1e620e5e3a68ba56810b0f39a914
LeeChengYou <901126jeff901126@gmail.com>
2024-06-14 17:42:07 +0800

    feat: Add api/items

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -1,69 +1,75 @@
 /** 
  * @author Cheng You Lee <901126jeff901126@gmail.com>
  */
+import Image from "next/image";
 export async function GET() {
-    // 1. get access token  
+    // 1. get access token
     const token = await getToken();
 
     // 2. get data from TDX api
-    if (token){
+    if (token) {
         const apiData = await fetchData(token);
-        return Response.json({apiData});
-    }else{
+        return Response.json(apiData);
+    } else {
         return Response.json(
-            {error: 'Fail to retrieve access token'},
-            {status:401}
+            { error: 'Failed to retrieve access token'},
+            { status: 401 }
         );
     }
 }
-async function getToken(){
+
+async function getToken() {
     const authUrl = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
     const params = new URLSearchParams();
-    params.append('grant_type','client_credentials');
-    params.append('client_id',process.env.TDX_CLIENT_ID);
-    params.append('client_secret',process.env.TDX_CLIENT_SECRET);
-    
+    params.append('grant_type', 'client_credentials');
+    params.append('client_id', process.env.TDX_CLIENT_ID);
+    params.append('client_secret', process.env.TDX_CLIENT_SECRET);
+
     try {
-        const response = await fetch(authUrl ,{
-            method :'POST',
-            headers:{
-                'Content-Type' : 'application/x-www-form-urlencoded',
+        const response = await fetch(authUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: params,
         });
-        if(response.ok){
+
+        if (response.ok) {
             const data = await response.json();
             //console.log(JSON.stringify(data));
             return data.access_token;
-        }else{
-            console.error("Error fetch token:",response.status);
+        } else {
+            console.error("Error fetching token:", response.status);
         }
-    } catch(error){
-        console.log("Error fetch token:",error);
+    } catch (error) {
+        console.log('Error fetching token:', error);
     }
+
     return null;
 }
-async function fetchData(token){
+
+async function fetchData(token) {
     const apiUrl = 'https://tdx.transportdata.tw/api/basic/v2/Tourism/ScenicSpot/YunlinCounty';
-    
-    try{
-        const response = await fetch(apiUrl,{
+
+    try {
+        const response = await fetch(apiUrl, {
             method: 'GET',
-            headers:{
-                Authorizatoin:`Bearer ${token}`,
-                'Content-Type':'application/json',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
             },
-    });
-    console.log(response);
-    if(response.ok){
-        const data = await response.json();
-        console.log(JSON.stringify(data));
-        return data;
-    }else{
-        console.error("Error fetch data:",response.status)
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(JSON.stringify(data));
+            return data;
+        } else {
+            console.error('Error fetching data:', response.status);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
-    } catch(error){
-        console.log("Error fetch data:",error);
-    }
+
     return null;
 }
